d1840dd7590c7ef1601a400279d1eab1dbd563fd
weslleycamposc2p1 <weslleycampos951@gmail.com>
2024-10-13 23:23:53 -0300

    .

---
+++ React/loja_musica/src/assets/Components/Header.jsx
@@ -0,0 +1,15 @@
+import Style from "../css/Header.module.css"
+
+function Header(){
+    return(
+        
+        <>
+            <div className={Style.navbar}>
+                <div></div>
+            </div>
+        </>
+
+    )
+}
+
+export default Header
