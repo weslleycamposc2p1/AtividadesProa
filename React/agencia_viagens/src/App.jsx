e1705c67f9214ae3342d2ad9f54534117044b639
weslleycamposc2p1 <weslleycampos951@gmail.com>
2024-10-14 22:13:47 -0300

    .

--- React/agencia_viagens/src/App.jsx
+++ React/agencia_viagens/src/App.jsx
@@ -1,11 +1,12 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './assets/Componentes/Header'
 import Home from './assets/Componentes/Home'
-import Escocia from './assets/Componentes/Escocia'
-import Grancanyon from './assets/Componentes/Grancanyon'
-import Muralha_china from './assets/Componentes/Muralha_china'
-import Aruba from './assets/Componentes/Aruba'
 import Rodape from './assets/Componentes/Rodape'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Escocia from './assets/Componentes/Escocia'
+import Muralha from './assets/Componentes/Muralha_china'
+import Grand from './assets/Componentes/Grand'
+import Aruba from './assets/Componentes/Aruba'
+
 import './App.css'
 
 function App() {
@@ -13,15 +14,15 @@ function App() {
 
   return (
     <>
-    <Router>
+      <Router>
           <Header />
           <Routes>
               <Route path='/' element={<Home />} />
-              <Route path='Escocia' element={<Escocia />} />
-              <Route path='Grancanyon' element={<Grancanyon />} />
-              <Route path='Muralhachina' element={<Muralha_china />} />
-              <Route path='Aruba' element={<Aruba />} />
-          </ Routes>
+              <Route path='/Escocia' element={<Escocia />} />
+              <Route path='/Grand' element={<Grand />} />
+              <Route path='/Muralhas' element={<Muralha />} />
+              <Route path='/Aruba' element={<Aruba />} />
+          </Routes>
           <Rodape />
       </Router>
     </>
